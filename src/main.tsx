ad7080e9a24201dcdbbaa6b83821b334d2fc2aeb
Pushparaj13811 <pushparajmehta002@gmail.com>
2025-05-08 08:31:18 +0545

    fix: add HelmetProvider to fix react-helmet-async errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 import { store } from './store';
 import RouterProvider from './routes';
 import './index.css';
@@ -8,7 +9,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider />
+      <HelmetProvider>
+        <RouterProvider />
+      </HelmetProvider>
     </Provider>
   </React.StrictMode>
 );
